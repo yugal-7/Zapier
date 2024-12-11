f419547f116ce605d0a6fd6a758a48caaa509cc9
Yugal Vashishth <yugalvashishth@Yugals-Laptop.local>
2024-12-11 22:00:40 +0530

    feat: add processor

---
+++ processor/src/index.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function main() {
+    while(1){
+        const pendingRows = await client.zapRunOutbox.findMany({
+            where: {},
+            take: 10
+        })
+        pendingRows.forEach(r => {
+            
+        })
+    }
+}
+
+main();
